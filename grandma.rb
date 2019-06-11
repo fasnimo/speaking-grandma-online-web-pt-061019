d196b292564a66281bd43fa119f94719dd6b7978
Rene Cocom <fasnimo@gmail.com>
2019-06-11 17:32:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
-else 
+    return "NO, NOT SINCE 1938!"
+  else 
+    return ""
 end
 end 
 
